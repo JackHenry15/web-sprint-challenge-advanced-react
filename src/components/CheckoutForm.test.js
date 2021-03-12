916ecb5fc1f169915a7e3d2ce79c6e8ce49611c6
Jack Henry <jackson.lewis.henry@gmail.com>
2021-03-12 14:03:43 -0500

    done with mvp, doing interview q's

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,41 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
+test('renders without errors', ()=>{
+    render(<CheckoutForm/>);
+});
 
-test("form header renders", () => {});
 
-test("form shows success message on submit with form details", () => {});
+test("form header renders", () => {
+    //arrange
+    render(<CheckoutForm/>);
+    //act
+    const header = screen.queryByText(/Checkout Form/i)
+    //assert
+    expect(header).toBeInTheDocument();
+});
+
+test("form shows success message on submit with form details", () => {
+    //arrange
+    render(<CheckoutForm/>);
+    //act
+    const firstInput = screen.getByLabelText("First Name:")
+    userEvent.type(firstInput, "Jeff");
+    const lastInput = screen.getByLabelText("Last Name:")
+    userEvent.type(lastInput, "Jefferson");
+    const addressInput = screen.getByLabelText("Address:")
+    userEvent.type(addressInput, "123 Alphabet Rd");
+    const cityInput = screen.getByLabelText("City:")
+    userEvent.type(cityInput, "New York");
+    const stateInput = screen.getByLabelText("State:")
+    userEvent.type(stateInput, "NY");
+    const zipInput = screen.getByLabelText("Zip:")
+    userEvent.type(zipInput, "12345");
+    const button = screen.getByRole("button");
+    userEvent.click(button)
+    //assert
+    expect(screen.getByTestId("successMessage")).toBeInTheDocument();
+});
